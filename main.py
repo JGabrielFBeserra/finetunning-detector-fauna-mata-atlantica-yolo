7836d66ac46eecbb55b27d881abc3d0be3e172a6
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2025-12-06 07:47:11 -0300

    feat: garantindo commit do dia

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
-model = YOLO('yolov8n.pt') # modelo de deteccao
+
 # model = YOLO('yolov8x-seg.pt') # modelo de segmentacao
 # model = YOLO('yolov8x-cls.pt') # modelo de classificao
 
